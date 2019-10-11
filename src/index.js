9219dbfd6bc5b69c9182df75afec86b6c137f76d
Eduardo Henrique <42817682+EduardoHenrique18@users.noreply.github.com>
2019-10-11 13:43:57 -0300

    Update uri

--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ const mongoose = require('mongoose');
 
 const routes = require('./routes');
 
-require('dotenv').config();
-
 const app = express();
 
 app.use(cors());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://kvothe:1123581321@stayfish-kzjb2.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 
 app.use(express.json());
 app.use(routes);
